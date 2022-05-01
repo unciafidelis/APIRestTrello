5132d21e3ac2fc8a93e60c6df962531fd5dd4676
unciafidelis <unciafidelis@gmail.com>
2022-05-01 10:37:41 -0500

    Add: app.js Trello add card request

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+if(!process.env.TOKEN && !process.env.KEY){
+  throw new Error('No hay configuración con Api Key y con Token')
+}
+
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+let cardTitle = `Card Nueva ${new Date()}`
+
+trello.addCard(cardTitle, "LaunchX Card Description", "6264e42be72d295e64f5c083",
+	function (error, trelloCard) {
+		if (error) {
+			console.log('Could not add card:', error);
+		}
+		else {
+			console.log('Added card:', trelloCard);
+		}
+	});
